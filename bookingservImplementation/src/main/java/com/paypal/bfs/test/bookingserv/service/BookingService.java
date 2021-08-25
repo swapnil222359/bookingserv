a14829a1595f561b391fb283b566b7d719755352
Swapnil Ingale <swapnilingale@Swapnils-MacBook-Air.local>
2021-08-25 15:19:56 +0530

    Adding documentation for Booking Service Interface

--- bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/service/BookingService.java
+++ bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/service/BookingService.java
@@ -5,8 +5,18 @@ import com.paypal.bfs.test.bookingserv.api.model.Booking;
 import java.util.List;
 
 public interface BookingService {
-
+    /**
+     * Create and persists {@link com.paypal.bfs.test.bookingserv.domain.BookingApplication}
+     *
+     * @param booking the booking object
+     * @return the created booking
+     */
     Booking create(Booking booking);
 
+    /**
+     * Retrieve and return all the {@link com.paypal.bfs.test.bookingserv.domain.BookingApplication} resources
+     *
+     * @return list of bookings
+     */
     List<Booking> getAllBookings();
 }
